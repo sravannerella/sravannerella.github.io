d46a137f6f23dee9bfd69037ffe0281c0d99c5c0
sravannerella <sravannerella@sravans-imac.attlocal.net>
2018-07-31 23:48:54 -0400

    initial commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LangsComponent } from './langs/langs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LangsComponent
   ],
   imports: [
     BrowserModule,
