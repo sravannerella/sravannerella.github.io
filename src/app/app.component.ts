f09d2ec8c7cced00df18fd33540063d4232adb11
sravannerella <sravannerella@sravans-imac.attlocal.net>
2018-08-09 00:02:17 -0400

    initial commit w/ slider projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '../../node_modules/@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  images= [
+    {img: "../assets/SyntaxPacket.png", name: "Syntax Packet", color: "primary", description: "A customizable modern file manager"},
+    {img: "../assets/flickmatics.png", name: "FlickMatics", color: "danger", description: "Personalized Movie Blogger for all"},
+    {img: "../assets/hashcode.png", name: "#Code", color: "violet", description: "A hackable text editor to your liking"}
+  ]
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
+
+
 }
